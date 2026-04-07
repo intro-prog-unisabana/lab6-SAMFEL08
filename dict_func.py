6f0a4e9202675f1a3cf25fba089f6084d0f79117
Samuel G <gonzalezsam451@gmail.com>
2026-04-07 09:54:15 -0500

    prueba

--- dict_func.py
+++ dict_func.py
@@ -1,9 +1,9 @@
 def employee_print(employee_info):
     info = employee_info.copy()
 
-    name = info.pop("Name", "N/A")
+    name   = info.pop("Name",   "N/A")
     salary = info.pop("Salary", "N/A")
-    role = info.pop("Role", "N/A")
+    role   = info.pop("Role",   "N/A")
 
     print(f"Name: {name}")
     print(f"Salary: {salary}")
