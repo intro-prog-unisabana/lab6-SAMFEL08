7a731b4d5ce8ba82ba727c035a9bc85361d4a219
Samuel G <gonzalezsam451@gmail.com>
2026-04-07 09:50:12 -0500

    prueba

--- dict_store.py
+++ dict_store.py
@@ -1,1 +1,11 @@
-# Write your code here!
+def temp_and_color(data):
+    temp_value = data.get("temp", None)
+    color_value = data.get("color", None)
+    return (temp_value, color_value)
+
+if __name__ == "__main__":
+    data1 = {"temp": 22.5, "color": "blue", "status": "ok"}
+    print(temp_and_color(data1))
+
+    data2 = {"status": "ok"}
+    print(temp_and_color(data2))
